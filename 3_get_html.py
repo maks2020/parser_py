74583c1b91c1f3789ab1d130bf3b444917145d1a
Maksim Spiridonov <wrkwrtr@gmail.com>
2017-02-16 13:24:25 +0400

    add comment and del not work code

--- 3_get_html.py
+++ 3_get_html.py
@@ -5,29 +5,26 @@ import pickle
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-from parser_01s.utils import get_html_make_file
+from parser_01s.utils import get_html_with_login
 from parser_01s.utils import read_file_in_list
 from parser_01s.utils import read_config
 
-
+# read config from directory of parse
 config_parse = {}
 config_parse = read_config()
 
+# define variable from dictionary configuration
 name_html = config_parse['name_html']
 catalog_results = config_parse['catalog_results']
 path_html_url = config_parse['path_things_url_file']
 path_output = config_parse['path_output_file']
+url_login = config_parse['url_login']
 num_file_parse = 1
 start_index = 0
 
+# define path for the source from the site
 path_output_file = (path_output + 'html_' +
                     name_html + '_' + str(num_file_parse) + '.parse')
 
-things_url_list = []
-with open(path_html_url) as input_source:
-    for item in input_source.readlines():
-        things_url_list.append(item.rstrip())
-
-
-get_html_make_file(read_file_in_list(path_html_url), path_output_file, 
-                    start_index)
+get_html_with_login(read_file_in_list(path_html_url), path_output_file,
+                    url_login, start_index)
