7edb4d10ff3e96eeb9645a60b8fae45a2af63671
Maksim Spiridonov <wrkwrtr@gmail.com>
2017-02-22 16:16:33 +0400

    add clean src and loop for size

--- parser_01s/mod_parse.py
+++ parser_01s/mod_parse.py
@@ -1,7 +1,6 @@
 # models parsing for sites
 
 import re
-
 from bs4 import BeautifulSoup
 
 
@@ -103,7 +102,8 @@ def parse_shilco_sport_v2(sources_html_list, url_site):
     rows_csv = []
     for source in sources_html_list:
         thing_data_list = []  # define list for data things
-        source_ed = source.replace(';', '')
+        #clear source from "; \n"
+        source_ed = source.replace(';', '').replace('\n', '')
         soup = BeautifulSoup(source_ed, "lxml")
         # soup = BeautifulSoup(sources_html_list[10].replace(';', ''), "lxml")
         # add common data for thing
@@ -121,6 +121,9 @@ def parse_shilco_sport_v2(sources_html_list, url_site):
         try:
             common_data['description'] = soup.find(
                 "span", class_="description").get_text(strip=True).replace('\n', '')
+            if '-->' in common_data['description']:
+                common_data['description'] = common_data[
+                    'description'].split('-->')[1]
         except:
             common_data['description'] = ''
         try:
@@ -162,49 +165,44 @@ def parse_shilco_sport_v2(sources_html_list, url_site):
                     data_thing_for_color = dict(
                         zip(keys_list_temp, values_list_temp))
                     thing_data_list.append(data_thing_for_color)
-        # string color: size/height...
+        #create dictionary item "color: size/height"
         thing_color_dict = {}
-        for item in thing_data_list[1:-1]:
+        for item in thing_data_list[1:]:
             thing_color_dict[item['0']] = ''
-        for item in thing_data_list[1:-1]:
+        for item in thing_data_list[1:]:
             thing_color_dict[item['0']] += item['1'] + '/' + item['2'] + ', '
         thing_color_size_height = ''
         for key in thing_color_dict:
             thing_color_size_height += (key + ': ' +
-                                        thing_color_dict[key][:-1])
-        # create sizes string
+                                        thing_color_dict[key][:-2]) + '. '
+        # create sizes list
         thing_sizes = {}
-        for item in thing_data_list[1:-1]:
+        for item in thing_data_list[1:]:
             thing_sizes[item['1']] = ''
         sizes_list = list(thing_sizes.keys())
         sizes_list.sort(reverse=True)
-        sizes_str = ''
-        for size in sizes_list:
-            sizes_str += size + ','
-        # find url images and create list
         as_list = soup.select("a[data-zoom-image]")
         imgs_url_list = []
+        #create list and string url images for .csv
         for item in as_list:
             imgs_url_list.append(url_site + item.get('data-zoom-image'))
         thing_data_list.append(imgs_url_list)  # add images in list data thing
-        # print(thing_data_list)
         # create row csv for url images
         imgs_urls = ''
         for img_url in thing_data_list[-1]:
             imgs_urls += img_url + ';'
-        # for item in thing_data_list[1:-1]:
-        # try:
-        row_csv = (thing_data_list[0]['part_number'] + ' ' +
-                   thing_data_list[0]['name_thing'] + ' ' + ';' +
-                   # thing_data_list[1]['0'] + ';' + #color
-                   thing_data_list[0]['part_number'] + ' ' +
-                   thing_data_list[0]['name_thing'] + ' ' + ';' +
-                   # thing_data_list[1]['0'] + ';' + #color
-                   thing_data_list[0]['description'] + ' ' +
-                   thing_color_size_height[:-1] + ';' +
-                   thing_data_list[0]['price_thing'] + ';' +
-                   sizes_str[:-1] + ';' + imgs_urls)
-        rows_csv.append(row_csv)
-    # except:
-        # ...
+        #create row csv on sizes
+        for size_thing in sizes_list:
+            row_csv = (thing_data_list[0]['part_number'] + ' ' +
+                       thing_data_list[0]['name_thing'] + ' ' + ';' +
+                       thing_data_list[0]['part_number'] + ' ' +
+                       thing_data_list[0]['name_thing'] + ' ' + ';' +
+                       thing_data_list[0]['description'] + ' ' +
+                       'Цвета, размер и рост доступные к заказу: ' +
+                       thing_color_size_height[:-1] + ';' +
+                       thing_data_list[0]['price_thing'] + ';' +
+                       # sizes_str[:-1] + ';' + imgs_urls)
+                       size_thing + ';' + imgs_urls)
+            #filling list rows_csv rows csv
+            rows_csv.append(row_csv)
     return rows_csv
